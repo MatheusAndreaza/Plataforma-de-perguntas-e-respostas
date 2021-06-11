044e474d75a574b1ab47300e89094f3f8a796049
Matheus Andreaza <matheus20203040@hotmail.com>
2021-06-10 21:10:16 -0300

    adiciona formulario

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.get('/perguntar', (req, res) => {
+    res.render('perguntar');
+});
+
+app.post('/salvarpergunta', (req, res) => {
+    const { titulo, descricao } = req.body;
+
+    console.log(titulo);
 });
 
 app.listen(3000);
