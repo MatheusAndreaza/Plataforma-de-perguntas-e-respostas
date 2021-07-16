b4fa3f2a27a692b7e7c620bc8b4a4c3dd3c84eac
Matheus Andreaza <matheus20203040@hotmail.com>
2021-07-15 22:34:56 -0300

    modifica banco

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
-const connection = new Sequelize('teste_server', 'teste_server', 'matheus10', {
-    host: 'mysql742.umbler.com',
-    port: '41890',
+const connection = new Sequelize('database', 'password', 'user', {
+    host: 'localhost',
+    port: '3306',
     dialect: 'mysql'
 });
 
